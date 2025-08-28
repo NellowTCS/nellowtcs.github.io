9b13cc2e16f8b34f44908b4a24ad0fd2cfa1df04
NellowTCS <neeljaiswal23@gmail.com>
2025-08-28 13:07:14 -0600

    Update histoire.config.js

--- histoire.config.js
+++ histoire.config.js
@@ -2,17 +2,18 @@ import { defaultColors, defineConfig } from 'histoire';
 import { HstSvelte } from '@histoire/plugin-svelte';
 
 export default defineConfig({
-	plugins: [HstSvelte()],
-	theme: {
-		title: 'Histoire',
-		logo: {
-			square: './static/favicons/favicon.png',
-			light: './static/favicons/favicon.png',
-			dark: './static/favicons/favicon.png'
-		},
-		favicon: './static/favicons/favicon.ico',
-		colors: {
-			primary: defaultColors.purple
-		}
-	}
+  base: '/stories/',
+  plugins: [HstSvelte()],
+  theme: {
+    title: 'Histoire',
+    logo: {
+      square: './static/favicons/favicon.png',
+      light: './static/favicons/favicon.png',
+      dark: './static/favicons/favicon.png'
+    },
+    favicon: './static/favicons/favicon.ico',
+    colors: {
+      primary: defaultColors.purple
+    }
+  }
 });
